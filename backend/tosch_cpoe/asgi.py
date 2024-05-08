fa61f0b585a409daeee4e5bbd98f784671a8dff5
Gaston Tognola <tognola08@gmail.com>
2024-05-08 15:08:44 +0200

    first installation

---
+++ backend/tosch_cpoe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tosch_cpoe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tosch_cpoe.settings')
+
+application = get_asgi_application()
